4745ff0acd72cc9158ac9f1ebb9f6bf82070d9b2
Tomas <tmgarcia@miuandes.cl>
2019-05-09 23:39:08 -0400

    primero

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,29 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+$(document).ready(function(){	
+
+	var write = $('#write');
+
+	$('#kcontainer .key').click(function(){
+		var write = $('#write');
+		var key = $(this),
+			character = key.html(); 
+		
+		// Delete
+		if (key.hasClass('key key-wide')) {
+			var html = write.html();
+			
+			write.html(html.substr(0, html.length - 1));
+			return false;
+		}
+		
+		if (key.hasClass('key-space')) character = ' ';
+		
+		if (key.hasClass('key-special key-wide')) character = "\n";
+		
+		// Add the character
+		
+		write.html(write.html() + character+'hola');
+		
+	});
 });
